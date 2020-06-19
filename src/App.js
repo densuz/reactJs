57f73550201412cab987f8f131ad30b74d6469da
densuz <densuz97@gmail.com>
2020-06-19 17:33:08 +0700

    #3 Membuat File BLog.jsx dan Blog.css (Props)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Fungsional/Home'
 import Beranda from './Component/Class/Beranda'
--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
     return <div>
-        <h1>Halaman Home</h1>
+        <Blog
+            Tanggal="16 juni 2020"
+            Judul="Deni"
+            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
+        />
+        <Blog
+            Tanggal="17 juni 2020"
+            Judul="Deni"
+            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
+        />
+        <Blog
+            Tanggal="18 juni 2020"
+            Judul="Deni"
+            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
+        />
+
     </div>
 }
 
