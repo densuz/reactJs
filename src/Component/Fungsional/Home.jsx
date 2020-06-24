1960da4298c0b3bfcac846cb2e243937fe05c354
densuz <densuz97@gmail.com>
2020-06-24 16:12:58 +0700

    #4 Selesai Membuat Produk.jsx dan Produk.css

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import Blog from './Blog'
+//import Blog from './Blog'
+import Produk from '../Class/produk'
 
 const Home = () => {
     return <div>
-        <Blog
-            Tanggal="16 juni 2020"
-            Judul="Deni"
-            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
-        />
-        <Blog
-            Tanggal="17 juni 2020"
-            Judul="Deni"
-            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
-        />
-        <Blog
-            Tanggal="18 juni 2020"
-            Judul="Deni"
-            Summary="pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya"
-        />
+            
+            <Produk nama="Realme Narzo" Harga="2.900.0000" stock="1"/>
+            <Produk nama="Realme 5 Pro" Harga="3.700.0000" stock="8"/>
+            <Produk nama="Razer Headset Gaming" Harga="7.900.0000" stock="10"/>
+            <Produk nama="Laptop Toshiba" Harga="8.900.0000" stock="17"/>
+            <Produk nama="Monitor SAMSUNG 30 inc" Harga="3.700.0000" stock="100"/>
+            <Produk nama="Mouse Razer Kraken" Harga="1.900.0000" stock="10"/>
+       
 
     </div>
 }
